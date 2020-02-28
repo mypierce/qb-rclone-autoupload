726054ab962c6abcf656e0f9fad1b5554954c24a
mypierce <myanata@gmail.com>
2020-02-28 21:59:21 +0800

    Update rcloneup.sh

--- rcloneup.sh
+++ rcloneup.sh
@@ -3,4 +3,4 @@ src=$1
 src=$src
 dst=od:oracle1upload/$2  #填写上传目的地的目录名
 echo  {$src} >/root/test.log    #上传文件日志输出
-rclone -v copy --ignore-existing "$src" "$dst" --log-file=/root/rclone.log       #rclone日志输出
+rclone -v copy --ignore-existing "$src" "$dst" --log-file=/root/rclone.log       #copy改move可以删除本地文件，rclone日志输出
